3c1e7a013eb3cf111c93c36ff61507ef31168df1
Jung Kim <jung.kim@amundi.com>
2024-09-02 01:34:32 +0200

    test commit minor change

--- cbr_final.py
+++ cbr_final.py
@@ -33,7 +33,7 @@ def calc_corr(macro_data: pd.DataFrame, asset_data: pd.DataFrame, asset_idx: int
     r2,c2= asset_data.shape
     macro_data=macro_data[-min(r1,r2):]
     # TEMP Solution as we have one more data point for SPX
-    asset_data=asset_data[-min(r1,r2)-1:-1]
+    asset_data=asset_data[-min(r1,r2):]
     # print(asset_data.shape)
     # print(macro_data.shape)
     corr_pearson = np.zeros((1,c1))
@@ -41,12 +41,16 @@ def calc_corr(macro_data: pd.DataFrame, asset_data: pd.DataFrame, asset_idx: int
     asset_val = asset_data.values
     
     for i in range(c1):
+        # print(i)
+        # print(macro_val[:,i].shape)
+        # print(asset_val[:,asset_idx].shape)
         corr_pearson[0,i] = stats.pearsonr(macro_val[:,i],asset_val[:,asset_idx]).correlation
     return corr_pearson
 
 corr_spx = calc_corr(macro_data=df_pctile_macro,asset_data=delta_assets_yoy, asset_idx=0)
-corr_spx_pct_df = pd.DataFrame(data=np.multiply(df_pctile_macro, np.abs(corr_spx.flatten().T)), index=delta_assets_yoy.index,columns=        df_pctile_macro.iloc[i,j] = stats.percentileofscore(delta_macro_yoy.iloc[:i+1,j],delta_macro_yoy.iloc[i,j])/100
-.columns)
+corr_spx_pct_df = pd.DataFrame(data=np.multiply(df_pctile_macro, np.abs(corr_spx.flatten().T)), index=delta_assets_yoy.index, columns=df_pctile_macro.columns)
+
 
 # corr_jpmtus = calc_corr(macro_data=macro_y,asset_data=asset_y, asset_idx=1)
 # corr_jpmtus_pct_df = pd.DataFrame(data=np.multiply(perc_y, np.abs(corr_jpmtus.flatten().T)), index=macro_y.index,columns=macro_y.columns)
+# %%
